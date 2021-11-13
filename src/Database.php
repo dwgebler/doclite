ce5da5ed0315b548d70c2a17b9ff9028b2512e85
dwgebler <davidgebler@gmail.com>
2021-11-13 00:04:51 +0000

    Bugfix version and changelog

--- src/Database.php
+++ src/Database.php
@@ -58,7 +58,7 @@ abstract class Database implements DatabaseInterface
      * Version number.
      * @var string
      */
-    private const VERSION = '1.1.2';
+    private const VERSION = '1.1.3';
     /**
      * DB connection
      * @var DatabaseConnection
